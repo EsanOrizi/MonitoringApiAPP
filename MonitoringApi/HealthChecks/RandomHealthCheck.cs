8026121f0894a42d5f3601fa5fab836f4b084767
Esan Alorizi <62729091+EsanOrizi@users.noreply.github.com>
2023-11-20 19:41:47 +0000

    API Health Checks

---
+++ MonitoringApi/HealthChecks/RandomHealthCheck.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace MonitoringApi.HealthChecks
+{
+    public class RandomHealthCheck : IHealthCheck
+    {
+        public Task<HealthCheckResult> CheckHealthAsync(
+            HealthCheckContext context, 
+            CancellationToken cancellationToken = default)
+        {
+            int responseTimeInMs = Random.Shared.Next(3000);
+            
+            if (responseTimeInMs < 1000)
+            {
+                return Task.FromResult(
+                    HealthCheckResult.Healthy($"response was excellent ({responseTimeInMs})"));
+            }
+            else if (responseTimeInMs < 2000)
+            {
+                return Task.FromResult(
+                    HealthCheckResult.Degraded($"response was more than expected ({responseTimeInMs})"));
+            }
+            else 
+            {   return Task.FromResult(
+                    HealthCheckResult.Unhealthy($"response was bad ({responseTimeInMs})"));
+            }
+
+        }
+    }
+}
--- MonitoringApi/Program.cs
+++ MonitoringApi/Program.cs
@@ -1,3 +1,7 @@
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using MonitoringApi.HealthChecks;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,17 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHealthChecks()
+    .AddCheck<RandomHealthCheck>("site health check")
+    .AddCheck<RandomHealthCheck>("database health");
+
+builder.Services.AddHealthChecksUI(opts =>
+{
+    opts.AddHealthCheckEndpoint("api", "/health");
+    opts.SetEvaluationTimeInSeconds(5);
+    opts.SetMinimumSecondsBetweenFailureNotifications(10);
+
+}).AddInMemoryStorage();
 
 var app = builder.Build();
 
@@ -21,5 +36,11 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapHealthChecks("/health", new HealthCheckOptions
+{
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+
+});
+app.MapHealthChecksUI();
 
 app.Run();
