50f8f99ad891232ebb4a0a7293279f2b69afe227
Esan Alorizi <62729091+EsanOrizi@users.noreply.github.com>
2023-11-20 19:17:01 +0000

    Add project files.

---
+++ MonitoringApi/Controllers/UsersController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MonitoringApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly ILogger<UsersController> logger;
+
+        public UsersController(ILogger<UsersController> logger)
+        {
+            this.logger = logger;
+        }
+
+        // GET: api/<UsersController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<UsersController>/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            //if (id < 0 || id > 100) {
+            //    logger.LogWarning("the id of {Id} was invallid", id);
+            //    return BadRequest("Index was out of range");
+            //}
+
+            //return Ok(id);
+            try
+            {
+                if (id < 0 || id > 100)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(id));
+                }
+                    return Ok(id);
+            } 
+            catch (Exception ex)
+            {
+                logger.LogWarning( ex, "the id of {Id} was invallid", id);
+                return BadRequest("Index was out of range");
+            }
+        }
+
+        // POST api/<UsersController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<UsersController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<UsersController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
